1029e3ff9ad9d953f2c7e5bdcb1280186c612cbe
Zoey Zheng <zoeyyandi@gmail.com>
2018-02-07 23:03:05 -0500

    initial

--- src/components/Titlebar.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Titlebar extends Component {
-  render() {
-    return <h1 className="titlebar">TO-DO LIST</h1>;
-  }
-}
-
-export default Titlebar;
--- src/constants/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const UPDATE_TODO = 'UPDATE_TODO';
-export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
-export const TOGGLE_EDITING = 'TOGGLE_EDITING';
-export const SHOW_ALL = 'SHOW_ALL';
-export const SHOW_COMPLETED = 'SHOW_COMPLETED';
-export const SHOW_ACTIVE = 'SHOW_ACTIVE';
-export const INITIAL_TODOLIST = 'INITIAL_TODOLIST';
